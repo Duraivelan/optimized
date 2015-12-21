840ff04b5f4d00a264b335ea9d86d3c4362090c9
Duraivelan Palanisamy <p.duraivelanece@gmail.com>
2015-12-21 16:17:57 +0100

    commented the force calculation routine to run the code faster

--- defs.h
+++ defs.h
@@ -6,7 +6,7 @@ const double m =1.0;
 const double inv_mass =1.0/m;
 const double dt=0.0001;
 const int NrParticles=xxNrParticles;
-const double r_cut  = pow(2.0,(1.0/6.0));
+const double r_cut  = 1.0 ; // pow(2.0,(1.0/6.0));
 const double r_cut2 = (r_cut)*(r_cut);
 const double sigma =1.0, epsilon =1.0;
 const double r_min = 1.0; //pow(2.0,(1.0/6.0))*sigma;
--- pairforce.h
+++ pairforce.h
@@ -4,22 +4,22 @@ if (particle[i].cluster!=particle[j].cluster)
 dr=particle[i].pos-(particle[j].pos+dR+shift*xxshift);
 r2=dr.norm2();
 
-		if (r2<(r_cut2)) 
+		if (r2<(r_cut2))
 		{
 		//	cout<<r2<<endl;
-			if(xxclustering && r2 < (r_min2)) 
+			if(xxclustering && r2 < (r_min2))
 				{
 					*combine_now += 1;
 					combine[	*combine_now	]	[ 0 ] = min(particle[i].cluster,particle[j].cluster);
-					combine[	*combine_now	]	[ 1 ] = max(particle[i].cluster,particle[j].cluster);					
+					combine[	*combine_now	]	[ 1 ] = max(particle[i].cluster,particle[j].cluster);
 					combine[	*combine_now	]	[ 2 ] = min(i,j);
 					combine[	*combine_now	]	[ 3 ] = max(i,j);
 				//	cout<<combine[*combine_now][0]<<'\t'<<combine[*combine_now][1]<<"insdide pairforce"<<'\t'<<*step<<endl;
 				}
 
 			 if (r2<(rs2)) {
-						    	r=std::sqrt(r2);								
-												
+						    	r=std::sqrt(r2);
+
 	// 							exponential potential from PHYSICAL REVIEW E VOLUME 50, NUMBER 3 SEPTEMBER 1994 Browniian dynamics simulations of self-difFusion and shear viscosity of near-hard-sphere colloids
 	//							F = n*epsilon*pow(sigma*sigma/rs,n)/rs; //  n is the exponent of the potential function
             					Fij=dr*Fs;
@@ -28,16 +28,16 @@ r2=dr.norm2();
 
 							//	if(cell[neighborList[i][j][k][m][0]][neighborList[i][j][k][m][1]][neighborList[i][j][k][m][2]][lC2] > cell[i][j][k][lC1]) {
 								*p_energy+= phis - phicut + Fs*(rs-r)*(rs-r);
-							} 
-			else {				
-									
+							}
+		/*	else {
+
 				r2inv=1.0/r2;
         		r6inv 	= 	r2inv*r2inv*r2inv;
         		r12inv 	= 	r6inv*r6inv;
 	//			simple potential
 	//			F=2*epsilon*(sigma-r)*rx/r;
 	//			*p_energy+=2*epsilon*(sigma-r)*(sigma-r);
-	
+
 	//			exponential potential from PHYSICAL REVIEW E VOLUME 50, NUMBER 3 SEPTEMBER 1994 Browniian dynamics simulations of self-difFusion and shear viscosity of near-hard-sphere colloids
 	//			F = n*epsilon*pow(sigma*sigma/r2,n/2)/r2; //  n is the exponent of the potential function
 	//			F = 4*epsilon*(12*pow(sigma*sigma/r2,6)-6*pow(sigma*sigma/r2,3))/r2; 	WCA potential
@@ -46,11 +46,11 @@ r2=dr.norm2();
 				particle[i].frc+=Fij;
 				particle[j].frc-=Fij;
 				*p_energy+=4.0*epsilon*(sigma12*r12inv-sigma6*r6inv) - phicut;
-			} 
-			
+			} */
+
 		}
 
-}  else {
+} /* else {
 
 if (r2<(r_cut2)) 
 		{
@@ -68,4 +68,4 @@ if (r2<(r_cut2))
 			} 
 			
 		}
-} 
+} */
